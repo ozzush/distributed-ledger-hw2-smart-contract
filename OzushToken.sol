24aa1ae6ea50a2ecddb4de2b63cca9418d43e863
Veronika Sirotkina <90182121+ozzush@users.noreply.github.com>
2025-05-03 21:49:41 +0200

    Update OzushToken.sol

--- OzushToken.sol
+++ OzushToken.sol
@@ -1,1 +1,20 @@
+pragma solidity 0.8.29;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract OzushToken is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("OzushToken", "OZH") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 18;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        require(to != address(0), "Cannot mint to zero address");
+        require(amount > 0, "Amount must be greater than zero");
+        _mint(to, amount);
+    }
+}
