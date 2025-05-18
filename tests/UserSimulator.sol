440ac951ead96699cde7a74a6fd30a44ba1473fa
Veronika Sirotkina <90182121+ozzush@users.noreply.github.com>
2025-05-18 20:51:27 +0200

    Update UserSimulator.sol

--- tests/UserSimulator.sol
+++ tests/UserSimulator.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.29;
 
+import "../contracts/OzushToken.sol";
+import "../contracts/OzushExchangeOffice.sol";
+
+contract UserSimulator {
+    OzushToken public token;
+    OzushExchangeOffice public office;
+
+    constructor(OzushToken _token, OzushExchangeOffice _office) {
+        token = _token;
+        office = _office;
+        token.approve(address(office), type(uint256).max);
+    }
+
+    function sell(uint256 amount) external {
+        office.sellTokens(amount);
+    }
+
+    receive() external payable {}
+}
